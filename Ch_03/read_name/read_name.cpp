5bbf99f786f1c29bc7b98c53937ffb58f5a715dd
ne_andrey <sorokinac@gmail.com>
2023-02-15 15:36:30 +0500

    read_name

---
+++ Ch_03/read_name/read_name.cpp
@@ -0,0 +1,11 @@
+//Считать и записать имя
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Введите ваше имя (и нажмите 'enter'):\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Привет, " << first_name << "!\n";
+	
+	return 0;
+}
