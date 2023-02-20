e541ba13bfea4c7cfc2410c72728e5db15ac29fe
ne_andrey <sorokinac@gmail.com>
2023-02-20 15:02:27 +0500

    vector_numb

--- Ch_04/exercise/6_vector_numb/vector_numb.cpp
+++ Ch_04/exercise/6_vector_numb/vector_numb.cpp
@@ -1,14 +1,25 @@
 #include "std_lib_facilities.h"
 
 int main(){
+
 	
 	vector<string> s_number = {"zero", "one", "two", "three", "four",
 		"five", "six", "seven", "eight", "nine"};
-
+/*
 	for (int temp; cin >> temp;){
 		cout << s_number[temp] << endl;
 	}
+	*/
+	
 
-
+	for (string temp; cin >> temp;){
+		int i = 0;
+		for(auto it : s_number){
+			if (it == temp)
+				cout << i << endl; 
+			++i;
+		}
+	}
+	
 	return 0;
 }
