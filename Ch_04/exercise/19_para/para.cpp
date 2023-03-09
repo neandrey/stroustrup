887a8ccd686f1eb84e6da704e923f9a9505d116a
ne_andrey <sorokinac@gmail.com>
2023-03-10 04:13:38 +0500

    para

--- Ch_04/exercise/19_para/para.cpp
+++ Ch_04/exercise/19_para/para.cpp
@@ -3,7 +3,30 @@
 
 int main(){
 
-	
+	vector<string> names;
+	vector<int> scores;
+	string name;
+	int score;
+
+	while(true){
+
+		cin >> name >> score;
+
+		if (name == "NoName" and score == 0)
+			break;
+		
+		for (auto n :names)
+			if (n == name)
+				error("Имя уже встречалось!");
+
+		names.push_back(name);
+		scores.push_back(score);
+
+	}	
+
+	for (size_t i = 0; i != names.size(); ++i)
+		cout << names[i] << ' ' << scores[i] << endl;
+
 
 	return 0;
 }
