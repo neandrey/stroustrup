7f4147a026200e3abb6526d43383ac6a027aa1ba
ne_andrey <sorokinac@gmail.com>
2023-03-09 21:28:03 +0500

    rock_scissors_paper

--- Ch_04/exercise/10_rock_paper_scissors/rock_paper_scissors.cpp
+++ Ch_04/exercise/10_rock_paper_scissors/rock_paper_scissors.cpp
@@ -1,32 +1,51 @@
 //rock, paper, scissors
 #include "std_lib_facilities.h"
 
+
 int main(){
 
-	vector<char> v_answer = {'r', 'p', 's'};
-	char c_val = 'a';
-	string ptr = "Введите камень, ножницы или бумага (r, p, s): ";	
-	int i = 3;
-	
-	while(v_answer.size()){
-		char answer = v_answer.back();
-		v_answer.pop_back();
+	vector<char> v_answer = {'r', 'p', 's', 's', 'r', 'p'};
+	char c_user = 'a';
 
-		cout << ptr;
-		switch (c_val){
+	string s_lose = "You lose";
+	string s_win = "You win";
+	string s_equal = "Once more";
+
+	for (auto c_comp : v_answer) {
+
+		cout << "Введите камень(s), ножницы(r) или бумага(p): ";	
+		cin >> c_user;
+
+		switch (c_user){
 			case 'r':
-				;
+				if (c_comp == 's')
+					cout << s_lose;
+				else if (c_comp == 'p')
+					cout << s_win;
+				else
+					cout << s_equal;
 				break;
 			case 'p':
-				;
+				if (c_comp == 'r')
+					cout << s_lose;
+				else if (c_comp == 's')
+					cout << s_win;
+				else
+					cout << s_equal;
 				break;
 			case 's':
-				;
+				if (c_comp == 'p')
+					cout << s_lose;
+				else if (c_comp == 'r')
+					cout << s_win;
+				else
+					cout << s_equal;
 				break;
 			default:
-				cout << "Не знаю такого значения " << c_val << endl;
+				cout << "Не знаю такого значения " << c_user;
 				break;
 		}
+		cout << endl;
 	}
 
 	return 0;
