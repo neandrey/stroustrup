dd1f45fa68b60d6536098230c71f7d16aea36f67
ne_andrey <sorokinac@gmail.com>
2023-03-10 19:17:36 +0500

    Name_value

---
+++ Ch_06/exercise/4_name_value/name_value.cpp
@@ -0,0 +1,38 @@
+//sqr_equation ax2 + b + c = 0
+#include "std_lib_facilities.h"
+
+class Name_value {
+public:
+	string name;
+	int score;
+	Name_value(string n, int s) : name(n), score(s) {}
+};
+
+int main(){
+
+	vector<Name_value> n_values;
+	string name;
+	int score;
+
+	while(true){
+
+		cout << "name and score (Joe 17): ";
+		cin >> name >> score;
+
+		if (name == "NoName" and score == 0)
+			break;
+		
+		for (auto n : n_values)
+			if (n.name == name)
+				error("Name already met!");
+
+		n_values.push_back(Name_value(name, score));
+
+	}	
+
+	for (auto n : n_values)
+		cout << n.name << ' ' << n.score << endl;
+
+	return 0;
+}
+//-------------------------------------------
