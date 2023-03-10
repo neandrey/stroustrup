5032a3c86c9e0215b95c35d005be91fff6469495
ne_andrey <sorokinac@gmail.com>
2023-03-10 20:49:39 +0500

    sentence

---
+++ Ch_06/exercise/6_is_sentence/eng_gramm.cpp
@@ -0,0 +1,18 @@
+//eng_grammar
+#include "std_lib_facilities.h"
+
+int main(){
+
+	string sentence;
+	getline(cin , sentence);
+
+	size_t found = sentence.find(" . ");
+
+	if (found != string::npos)
+		cout << "OK" << endl;
+	else
+		cout << "not OK" << endl;
+
+	return 0;
+}
+//-------------------------------------------
