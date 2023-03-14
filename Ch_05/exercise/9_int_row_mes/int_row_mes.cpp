cc8235631691652da8d0c9b6481edb1f789cbc4a
ne_andrey <sorokinac@gmail.com>
2023-03-14 07:12:02 +0500

    int_row_message

---
+++ Ch_05/exercise/9_int_row_mes/int_row_mes.cpp
@@ -0,0 +1,38 @@
+//int_row_message
+#include "std_lib_facilities.h"
+
+
+//--------------------------------------------
+int main(){
+	
+	int n, sum = 0;
+	vector<int> i_val;	
+	string s_val;
+	cout << "Введите количество суммируемых значений: ";
+	cin >> n;
+	if(!cin)
+		error("Результат не может быть представлен в виде int");
+
+	cout << "Введите несколько целых чисел (| для окончания ввода): ";
+	for(int temp; cin >> temp; ){
+		if(!cin)
+			error("Результат не может быть представлен в виде int");
+		i_val.push_back(temp);
+	}
+
+	if(i_val.size() < n)
+		error("Выход за границы вектора!");
+	else
+		for (size_t i = 0; i != n; ++i){
+			sum += i_val[i]; 
+			if(i+1 == n)
+				s_val += to_string(i_val[i]);
+			else
+				s_val += to_string(i_val[i]) + ' ';
+		}
+		
+	cout << "Сумма первых " << n << " чисел (" << s_val << ") равна " << sum;
+	cout << endl;	
+		
+	return 0;
+}
