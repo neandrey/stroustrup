75183b14f0ff95f46232eda1d6cfa9fdb22e6b07
ne_andrey <sorokinac@gmail.com>
2023-03-31 16:43:57 +0500

    drill

--- Ch_10/exercise/drill/Point.cpp
+++ Ch_10/exercise/drill/Point.cpp
@@ -4,43 +4,49 @@ namespace my_point{
 
 Point::Point() : x{0}, y{0}
 {}
-
+//--------------------------------------------------
 Point::Point(int xx, int yy) : x{xx}, y{yy}
-{
-    //if(!is_correct(x, y)) throw Invalid(); 
-}
-
+{}
+//--------------------------------------------------
 void Point::set_point(int x, int y){
     this->x = x;
     this->y = y;
 }
-
+//---------------------------------------------------
 Point Point::get_point() const {
 
     return Point(x, y);
 }
-
-ostream &operator<<(ostream &os, const Point &p){
-
-    os << "x = " << p.get_x() << " y = " << p.get_y();
+//---------------------------------------------------
+ostream &operator<<(ostream &os, const Point &p)
+{
+    os << "(" << p.get_x() << ", " << p.get_y() << ")";
     return os;
 }
-
+//---------------------------------------------------
 istream &operator>>(istream &is, Point &p){
+    
+    char ch1;
+    if(is >> ch1 && ch1 != '('){
+        is.unget();
+        is.clear(ios_base::failbit);
+        return is;
+    }
 
+    char ch2, ch3;
     int x, y;
-    cout << "Введите x и y: ";
-    is >> x >> y;
-    if(!is) return is;
+    is >> x >> ch2 >> y >> ch3;
+    if(!is || ch3 != ')'){ 
+        if(is.eof()) return is;
+        error("Неправильный ввод");
+    }
 
     p.set_x(x);
     p.set_y(y);
 
     return is; 
-
 }
-
-
+//----------------------------------------------------
 }
 
 
--- Ch_10/exercise/drill/main_point.cpp
+++ Ch_10/exercise/drill/main_point.cpp
@@ -4,16 +4,41 @@
 using namespace my_point;
 int main(int argc, char const *argv[])
 {
-    const int size_points = 7;
+    const int size_points = 3;
     vector<Point> original_points;   
     Point p;
 
     for(size_t i = 0; i != size_points; ++i){
+        cout << "Введите (x,y): ";
         cin >> p;
         original_points.push_back(p);
     }
 
     for(auto p : original_points)
+        cout << p << endl;
+    cout << endl;
+
+    string oname = "point.txt";
+    //cin >> oname;
+    ofstream ofs{oname};
+    if(!ofs) error("Невозможно открыть выходной файл ", oname);
+
+    for(auto p : original_points)
+        ofs << p << endl;
+    
+    vector<Point> processed_point;
+    ifstream ifs{oname};
+    if(!ifs) error("Невозможно открыть входной файл ", oname);
+
+    ifs.exceptions(ifs.exceptions() | ios_base::badbit);
+
+    while(true){
+        Point p;
+        if(!(ifs>>p)) break;
+        processed_point.push_back(p);
+    }
+
+    for(auto p : processed_point)
         cout << p << endl;
 
     return 0;
