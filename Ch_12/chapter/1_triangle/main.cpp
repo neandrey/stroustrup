851da247549ac61dbe5713fd670a817ebb188f4f
ne_andrey <sorokinac@gmail.com>
2023-04-10 14:41:22 +0500

    1_triangle

---
+++ Ch_12/chapter/1_triangle/main.cpp
@@ -0,0 +1,24 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+    Point tl(100, 100);
+
+    Simple_window win(tl, 400, 600, "Canvas");
+     
+
+    Polygon poly;
+
+    poly.add(Point( 300, 200 ));
+    poly.add(Point( 350, 100 ));
+    poly.add(Point( 400, 200 ));
+
+    poly.set_color(Color::red);
+
+    win.attach(poly);
+
+    win.wait_for_button();
+}
