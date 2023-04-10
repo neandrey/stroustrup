55f12115cb2ea7e9ec3e176678bbb0c7ea084673
ne_andrey <sorokinac@gmail.com>
2023-04-10 15:36:17 +0500

    3_sin

---
+++ Ch_12/chapter/3_sin/main.cpp
@@ -0,0 +1,34 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+
+    Point t1 {100, 100};
+    Simple_window win(t1, 600, 600, "Canvas");
+
+    Axis xa{Axis::x, Point(20, 300), 280, 10, "x axis"};
+    //Axis::x - горизонтальная ось
+    //Начало оси - 20, 300
+    //Длина оси - 280
+    //Число делений - 10
+    //Метка оси - "x axis"
+
+    Axis ya{Axis::y, Point(20, 300), 280, 10, "y axis"};
+    ya.set_color(Color::cyan);
+    ya.label.set_color(Color::dark_red);
+
+    Function sine{sin, 0, 100, Point{20, 150}, 1000, 50, 50};
+    //sin() в диапазоне [0, 100) от (0, 0) до (20, 150)
+    //Используя 1000 точек
+    //50 для масштабирования координаты
+
+    win.attach(ya);
+    win.attach(xa);
+    win.attach(sine);
+    win.set_label("Canvas #2");
+
+    win.wait_for_button();
+}
