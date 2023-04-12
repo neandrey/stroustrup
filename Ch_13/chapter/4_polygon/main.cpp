bec0e7d4f7f856e4c51f026b4e5a70430bd61d17
ne_andrey <sorokinac@gmail.com>
2023-04-13 00:35:39 +0500

    4_polygon

--- Ch_13/chapter/4_polygon/main.cpp
+++ Ch_13/chapter/4_polygon/main.cpp
@@ -25,6 +25,22 @@ int main()
     rect11.set_fill_color(Color::blue);
     rect12.set_fill_color(Color::red);
     rect21.set_fill_color(Color::green);
+
+    //сдвиг прямоугольника rect11
+    rect11.move(400, 0);
+    rect11.set_fill_color(Color::white);
+    win.set_label("rectangles 2");
+
+    //перемещение слоев
+    win.put_on_top(rect00);
+    win.set_label("rectangels 3");
+
+    //удаление границ прямоугольников
+    rect00.set_color(Color::invisible);
+    rect11.set_color(Color::invisible);
+    rect12.set_color(Color::invisible);
+    rect21.set_color(Color::invisible);
+    rect22.set_color(Color::invisible);
     
     //win.attach(poly);
     win.attach(rect00);
