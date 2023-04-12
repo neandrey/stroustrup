c058d47e03bd0582f2e8f093e9e2a15502ccf26c
ne_andrey <sorokinac@gmail.com>
2023-04-13 02:17:38 +0500

    10_Image

---
+++ Ch_13/chapter/10_Image/main.cpp
@@ -0,0 +1,23 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+#include "../GUI/std_lib_facilities.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+
+    Point x{100, 100};
+
+    Simple_window win(x, 600, 400, "Image_Rita");
+
+    Image rita {Point{0,0}, "../GUI/rita.jpg"};
+    Image path {Point{0,0}, "../GUI/rita_path.gif"}; 
+
+    path.set_mask(Point{50, 250}, 600, 400);
+
+    win.attach(path);
+    win.attach(rita);
+
+    win.wait_for_button();
+}
