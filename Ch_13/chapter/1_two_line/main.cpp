27dd8830db5c0abea40cfba009cd27f6205d3e73
ne_andrey <sorokinac@gmail.com>
2023-04-12 17:00:04 +0500

    1_two_lines

---
+++ Ch_13/chapter/1_two_line/main.cpp
@@ -0,0 +1,28 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+#include "../GUI/std_lib_facilities.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+
+    Point x{100, 100};
+
+    Simple_window win(x, 600, 400, "Two lines");
+     
+    //Рисуем две линии
+    // Line horizontal{x, Point{200, 100}};
+    // Line vertical{Point{150, 50}, Point{150, 150}};
+
+    //c помощью калсса Lines
+    Lines x_lines;
+    x_lines.add(Point{100, 100}, Point{200, 100});
+    x_lines.add(Point{150, 50}, Point{150, 150});
+
+
+    // win.attach(horizontal);
+    // win.attach(vertical);
+    win.attach(x_lines);
+    win.wait_for_button();
+}
