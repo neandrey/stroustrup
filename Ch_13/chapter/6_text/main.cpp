7008b040c0614ddf6cbef7e7c373805391199cb6
ne_andrey <sorokinac@gmail.com>
2023-04-13 01:09:11 +0500

    6_text

---
+++ Ch_13/chapter/6_text/main.cpp
@@ -0,0 +1,19 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+#include "../GUI/std_lib_facilities.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+
+    Point x{100, 100};
+
+    Simple_window win(x, 600, 400, "Text");
+
+    Text t(Point(200, 200), "A closed polyline that isn't a polygon");
+    t.set_color(Color::blue);
+
+    win.attach(t); 
+    win.wait_for_button();
+}
