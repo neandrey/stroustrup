d1a26a8c8ae3bb1edf179fffa2dc782da9fb1d7f
ne_andrey <sorokinac@gmail.com>
2023-04-12 23:31:59 +0500

    4_polyline

---
+++ Ch_13/chapter/3_polyline/main.cpp
@@ -0,0 +1,25 @@
+#include "../GUI/Simple_window.h"
+#include "../GUI/Graph.h"
+#include "../GUI/std_lib_facilities.h"
+
+int main()
+{
+    using namespace Graph_lib;
+
+
+    Point x{100, 100};
+
+    Simple_window win(x, 600, 400, "Polyline");
+
+    Open_polyline opl = {
+        {100, 100}, {150, 200}, {250, 250}, {300, 200}
+    };
+
+    Closed_polyline cpl = {
+        {100, 100}, {150, 200}, {250, 250}, {300, 200}
+    };
+    
+    win.attach(opl);
+    win.attach(cpl);
+    win.wait_for_button();
+}
