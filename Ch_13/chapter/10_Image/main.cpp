9d242f4971b6bbbed15d890ce5acccdaee0ad957
ne_andrey <sorokinac@gmail.com>
2023-04-17 21:49:44 +0500

    10_Image

--- Ch_13/chapter/10_Image/main.cpp
+++ Ch_13/chapter/10_Image/main.cpp
@@ -11,8 +11,8 @@ int main()
 
     Simple_window win(x, 600, 400, "Image_Rita");
 
-    Image rita {Point{0,0}, "../GUI/rita.jpg"};
-    Image path {Point{0,0}, "../GUI/rita_path.gif"}; 
+    Image rita {Point{0,0}, "../../GUI/rita.jpg"};
+    Image path {Point{0,0}, "../../GUI/rita_path.gif"}; 
 
     path.set_mask(Point{50, 250}, 600, 400);
 
