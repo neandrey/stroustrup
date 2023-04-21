e9ed66fcb1633c0b42df6f00245a0d24d38ee2e8
ne_andrey <sorokinac@gmail.com>
2023-04-22 03:00:42 +0500

    2_out_two_string

---
+++ Ch_02/exercise/1_out_two_string/hello_world.cpp
@@ -0,0 +1,12 @@
+//Выводит две строки
+//Hello, programming!
+//Here we go!
+
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Hello, programming!" << endl;
+	cout << "Here we go!" <<  endl;
+	
+	return 0;
+}
