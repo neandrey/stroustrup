5756320414d43cdc3056990ff56cd29d833d4443
ne_andrey <sorokinac@gmail.com>
2023-04-22 02:10:21 +0500

    3_to_lower

---
+++ Ch_17/exercise/3_to_lower/main.cpp
@@ -0,0 +1,44 @@
+//dynamic_memory
+#include "std_lib_facilities.h" 
+
+void to_lower(char* s);
+int chr_to_int(char s);
+char int_to_chr(int n);
+
+int main(){
+
+    char s[] = "Hello, World!";
+
+    to_lower(s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+void to_lower(char *s)
+{
+
+    if(s == nullptr)
+        return;
+        
+    while(*s != '\0'){
+        if(*s >= 'A' and *s <= 'Z'){
+            *s = int_to_chr(chr_to_int(*s) + 32);
+        }
+        ++s;
+    }
+    
+}
+
+int chr_to_int(char s)
+//преобразуем char->int
+{
+    return int(s);
+}
+
+char int_to_chr(int n)
+//преобразуем int->char
+{
+    return char(n);
+}
