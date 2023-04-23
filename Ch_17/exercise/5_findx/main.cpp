f195dbaf14354294e7d781414c1fa4f1ef20ea01
ne_andrey <sorokinac@gmail.com>
2023-04-23 20:08:57 +0500

    5_findx

--- Ch_17/exercise/5_findx/main.cpp
+++ Ch_17/exercise/5_findx/main.cpp
@@ -2,17 +2,18 @@
 #include "std_lib_facilities.h" 
 
 char* findx(const char* s, const char* x);
-
+pair<int, int> first_and_len(const char* s, const char* x);
 
 int main(){
 
     char* s = "Hello, World!";
-    char* find_s = "Wor"; 
+    char* find_s = "Work"; 
     char* res; 
 
     res = findx(s, find_s);
 
     cout << res << endl;
+    delete[] res;
 
     return 0;
 }
@@ -21,30 +22,42 @@ char* findx(const char* s, const char* x){
 // нахождение первого вхождения   
 
     if(!s) return "";
-    size_t v1{0}, v2{0}, len{0}, h{0};
+    pair<int, int> fe_pair;
+
+    fe_pair = first_and_len(s, x);
+    
+    //cout << fe_pair.first << ' ' << fe_pair.second << endl;
+    size_t len = fe_pair.second;
+    size_t first = fe_pair.first;
+
+    if(!len)
+        return "";
+
+    char* c_find = new char[len];
+
+    for (size_t i = 0; i < len; ++i){
+        c_find[i] = s[first++];
+    }
+    c_find[len] = '\0';
+    
+    return c_find;
+}
+
+pair<int, int> first_and_len(const char *s, const char* x)
+//первое совпадение и длина строки 
+{
+    size_t first{0}, len{0}; 
     bool f_first{true};
 
     for (size_t i = 0; s[i]; ++i){
-        //cout << s[i] << ' ' << x[h] << endl;
-        if(s[i] == x[h]){
+        //cout << s[i] << ' ' << x[len] << endl;
+        if(s[i] == x[len]){
             if(f_first){
-                v1 = i;
+                first = i;
                 f_first = false;
             }
-            ++h;
+            ++len;
         }
     }
-    len = h;
-    v2 = v1 + len;
-    cout << v1 << ' ' << v2 << ' ' << len << endl;
-
-    char* c_find = new char[len+1];
-
-    size_t iiter{0};
-    for (v1; v1 < v2; ++v1){
-        c_find[iiter++] = s[v1];
-    }
-    c_find[len+1] = '\0';
-    
-    return c_find;
+    return pair<int, int>(first, len);
 }
