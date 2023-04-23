292be2d34dc80d3d4de1419e37e664d7d8d19e59
ne_andrey <sorokinac@gmail.com>
2023-04-23 22:16:53 +0500

    no_memory

--- Ch_17/exercise/6_no_memory/main.cpp
+++ Ch_17/exercise/6_no_memory/main.cpp
@@ -1,35 +1,19 @@
 //переполнение выделенной памяти из кучи. 
 #include "std_lib_facilities.h" 
 
-void size_double(); //размер типа double
-size_t memory_size(int n); //сколько памяти выделено
-
 int main(){
 
-    size_t max = 10000;
-    size_double();
+    size_t megabyte = 1048576;
+    size_t i{0}, max{100};
 
+    //Arch выжирает всю свободную память после чего приходится делать reboot
     while(true){
 
-        double* dd = new double[max];
-        cout << "Количество выделенной памяти: " << memory_size(max) 
-        << " байт" << endl;
+        char* dd = new char[megabyte * max];
+        cout << "Количество выделенной памяти: " << (i+=max) 
+            << " Mбайт" << endl;
     }
 
     return 0;
 }
 
-void size_double()
-//сколько памяти занимает double
-{
-    cout << "размер типа double в байтах = " << sizeof(double) << endl;
-}
-
-size_t memory_size(int n)
-//количество выделенной памяти
-{
-    static size_t res{0};
-    res += sizeof(double) * n;
-
-    return res;
-}
