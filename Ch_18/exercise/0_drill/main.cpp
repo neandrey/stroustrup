229fcdedf25b9b56e3d3394b6a5b9fcf0cf17f99
ne_andrey <sorokinac@gmail.com>
2023-05-11 20:31:39 +0500

    0_drill

--- Ch_18/exercise/0_drill/main.cpp
+++ Ch_18/exercise/0_drill/main.cpp
@@ -1,10 +1,99 @@
-//palyndrom. 
+//0_drill 
 #include "std_lib_facilities.h" 
 
-//1 global array
-int ga[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+// array 
+const int length = 10;
+int ga[length] = {1, 2, 4, 8, 16, 32, 64, 128, 512, 1024};
 
-int main(){
+void print_arr(const int arr[], int l);
+void f(int arr[], int len)
+{
+    int la[length];
+
+    //copy ga -> la
+    for(size_t i = 0; i != length; ++i)
+        la[i] = ga[i];
+    
+    //print la
+    cout << "print la: ";
+    print_arr(la, length);
+
+    //указатель p
+    int* p = new int[len];
+
+    for(size_t i = 0; i !=len; ++i)
+        p[i] = arr[i];
+
+    //print p
+    cout << "print p: ";
+    print_arr(p, len);
+
+    delete[] p;
+}
+
+void print_arr(const int arr[], int l){
+//печать массива
+    for(size_t i = 0; i != l; ++i)
+        cout << arr[i] << ' ';
+    cout << endl;
+}
+
+void init_arr(int arr[], int l){
+//заполняем факториал
+    int sum = 1;
+    for(size_t i = 1 ; i <= l; ++i){
+        arr[i-1] = sum *= i;
+    }
+}
+
+// vector
+void print_vec(const vector<int>& v);
+void init_vector(vector<int>& v);
+vector<int> gv{1, 2, 4, 8, 16, 32, 64, 128, 512, 1024};
+
+void f(vector<int> v)
+{
+
+    vector<int> lv;
+    for(size_t i = 0; i != gv.size(); ++i)
+        lv.push_back(gv[i]);
+
+    cout << "print lv: ";
+    print_vec(lv);
+
+    vector<int> lv2 = v;
+
+    cout << "print lv2: ";
+    print_vec(lv2);
+}
+
+void print_vec(const vector<int>& v){
+    
+    for(size_t i = 0; i != v.size(); ++i)
+        cout << v[i] << ' ';
+    cout << endl;
+}
+
+void init_vector(vector<int>& v){
+//инициализация вектора
+    int sum = 1;
+    for(size_t i = 1; i <= v.size(); ++i)
+        v[i-1] = sum *= i;
+}
+
+int main()
+{
+    //array
+    f(ga, length);
+    int aa[length];
+    init_arr(aa, length);
+    f(aa, length);
+
+    //vector
+    f(gv);
+    vector<int> vv(10, 0);
+    init_vector(vv);
+    f(vv);
 
     return 0;
 }
