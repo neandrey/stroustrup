639b4a7c7f2269daa22e71b05eafba601debbbe2
ne_andrey <sorokinac@gmail.com>
2023-05-15 17:59:27 +0500

    1_error_area_rect

--- Ch_05/chapter/1_error_area_rectangle/area.cpp
+++ Ch_05/chapter/1_error_area_rectangle/area.cpp
@@ -1,32 +1,35 @@
 #include "std_lib_facilities.h"
 
-
-class Bad_area {};
-//Площадь прямоугольника
 int area(int length, int width){
-    if(length<=0 || width <=0)
-        throw Bad_area{}; 
+//площадь прямоугольника
+
+    if(length <=0 or width <= 0)
+        error("Неположительный аргумент area()");
     return length * width;
 }
-//Площадь в пределах рамки
-int framed_area(int x, int y){
-    return area(x-2, y-2);
-}
-//-------------------------------------
-int main(int argc, char const *argv[])
-{
-    try {
-        int x = -1;
-        int y = 2;
-        int z = 4;
 
-        int area1 = area(x, y);
-        int area2 = framed_area(1, z);
-        int area3 = framed_area(y, z);
-        double ratio = double(area1) / area3;
-    }catch(Bad_area) {
-        cout << "Неверный аргумент функции area()\n";
-    }
-    
+int framed_area(int x, int y){
+//площадь в пределах рамки
+
+    constexpr int frame_width = 2;
+    if(x-frame_width <= 0 or y-frame_width <= 0)
+        error("Неположительный аргумент функции area()"
+        "при вызове из функции framed_area()");
+    return area(x-frame_width, y-frame_width);
+}
+
+int main(){
+
+    int x = -1;
+    int y = 2;
+    int z = 4;
+
+    int area1 = area(x, y);
+    int area2 = framed_area(1, z);
+    int area3 = framed_area(y, z);
+    double ratio = double(area1) / area3;
+
+    cout << ratio << endl;
+
     return 0;
 }
